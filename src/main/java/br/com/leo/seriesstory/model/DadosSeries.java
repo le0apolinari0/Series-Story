12519520e7b3343b83472227eb98658e79034033
le0apolinari0 <leosantosap@outlook.com>
2024-02-28 15:48:46 -0300

    Utilizando conceito em lambdas

--- src/main/java/br/com/leo/seriesstory/model/DadosSeries.java
+++ src/main/java/br/com/leo/seriesstory/model/DadosSeries.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosSeries(@JsonAlias("Title") String titulo ,
-                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                          @JsonAlias("imdbRating") String avaliacao) {
+public record DadosSeries(
+                          @JsonAlias("Title") String titulo,
+                          @JsonAlias("imdbRating") String avaliacao,
+                          @JsonAlias("totalSeasons") Integer totalTemporadas) {
 }
---
+++ src/main/java/br/com/leo/seriesstory/service/SeriesstoryApplication.java
@@ -0,0 +1,24 @@
+package br.com.leo.seriesstory.service;
+
+import br.com.leo.seriesstory.Controller.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SeriesstoryApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesstoryApplication.class, args);
+	}
+	@Override
+	public void run( String...args) throws Exception{
+
+		Principal principal = new Principal();
+		principal.mostrarMenu();
+	}
+
+}
+
